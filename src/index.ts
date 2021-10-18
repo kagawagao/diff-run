19ff80c0c7fb8ddbe81ba024f62845fa62e073c3
kagawagao <kingsongao1221@gmail.com>
2021-10-18 16:21:52 +0800

    fix: fix exec output

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,50 @@
 import debug from './debug'
-import { execSync } from 'child_process'
+import { exec } from 'child_process'
 import signale from 'signale'
 import chalk from 'chalk'
+import { promisify } from 'util'
 import { getRepoInfo } from './utils'
 
-type DiffRunOptions = Record<string, any>
-
-const getChangeset = () => {
-  debug('get git changeset')
-  try {
-    const changeset = execSync(
-      'git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD',
-      {
-        stdio: 'ignore',
-      }
-    )
-
-    return changeset
-  } catch (error) {
-    signale.error(
-      'get changeset error: %s',
-      chalk.red((error as Error).message)
-    )
-    return []
-  }
+export interface DiffRunOptions {
+  debug?: boolean
 }
 
-const diffRun = async (options?: DiffRunOptions) => {
-  debug('diff-run started')
+const getChangeset = async () => {
+  debug('get git changeset')
 
   const repoInfo = await getRepoInfo()
 
+  debug('repo info:', JSON.stringify(repoInfo, null, 2))
   if (!repoInfo.sha) {
     signale.info('No reversion found in current repo')
   } else {
-    const changeset = getChangeset()
+    try {
+      const { stdout } = await promisify(exec)(`git rev-list --count HEAD`)
+      const commitCount = parseInt(stdout.trim())
+      if (commitCount < 2) {
+        return []
+      }
+      const { stdout: changeset } = await promisify(exec)(
+        'git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD'
+      )
 
-    console.log(changeset)
+      return changeset
+    } catch (error) {
+      signale.error(
+        'get changeset error: %s',
+        chalk.red((error as Error).message)
+      )
+      return []
+    }
   }
+}
+
+const diffRun = async (options: DiffRunOptions = {}) => {
+  debug('diff-run started')
+
+  const changeset = await getChangeset()
+
+  console.log(changeset)
 }
 
 export default diffRun
